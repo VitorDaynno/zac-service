7a8f6299cf4328f5d43d36c6cbcf33a67469d59d
VitorDaynno <vitordaynno@gmail.com>
2023-11-04 15:43:26 -0300

    Adding route to create routines

--- main.py
+++ main.py
@@ -4,12 +4,21 @@ from src.config.logger import logger
 from src.helpers.thread_job import ThreadJob
 from src.server import http_server
 from src.services.trello_crawler import TrelloCrawler
+from src.factories.factory_controller import FactoryController
 
 
 def updated_cards():
     logger.info("Initializing updated cards")
     trello_crawler = TrelloCrawler()
     trello_crawler.integrate_cards()
+
+def create_tasks_by_routine():
+    logger.info("Initializing create tasks by routine")
+
+    factoryController = FactoryController()
+    task = factoryController.get_task()
+
+    task.create_tasks_by_routine()
 
 
 def main():
@@ -18,6 +27,10 @@ def main():
     event = threading.Event()
     routine = ThreadJob(updated_cards, event, 86400)
     routine.start()
+
+    routine_event = threading.Event()
+    routine_cron = ThreadJob(create_tasks_by_routine, routine_event, 60)
+    routine_cron.start()
 
     http_server.serve_forever()
 
--- src/controllers/routine.py
+++ src/controllers/routine.py
@@ -1,12 +1,89 @@
 from src.config.logger import logger
+from src.helpers.error_helper import UnprocessableEntity
 
 
 class Routine:
-    def __init__(self, dao, date_helper):
+    def __init__(self, dao, date_helper, model_helper):
         logger.info("Initializing Routine")
         self._dao = dao
+        self._date_helper = date_helper
+        self._model_helper = model_helper
 
     def get(self, filters):
         logger.info("Initializing get routine")
         routines = self._dao.get(filters)
         return routines
+
+    def create(self, user, entity):
+        try:
+            logger.info("Saving routine '{0}'".format(entity))
+
+            self.verify(entity)
+
+            routine = {
+                "name": entity["name"],
+                "days": entity["days"],
+                "start_time": entity["startTime"],
+                "end_time": entity["endTime"],
+                "user_id": user["id"],
+                "is_active": True,
+                "is_enabled": True
+            }
+
+            id = self._dao.insert(routine)
+
+            created_routine = self.get_by_id(id)
+
+            return self._model_helper.zac_routines(created_routine)[0]
+        except Exception as error:
+            logger.error("An error occurred: {0}".format(error))
+            raise error
+
+    def verify(self, routine):
+        if "name"  not in routine:
+            raise UnprocessableEntity("name is required")
+
+        if "days" not in routine:
+            raise UnprocessableEntity("days is required")
+
+        if "startTime" not in routine:
+            raise UnprocessableEntity("startTime is required")
+
+        if "endTime" not in routine:
+            raise UnprocessableEntity("endTime is required")
+
+    def get_pending_routines(self):
+        logger.info("Starting get pending routines")
+
+        today = self._date_helper.initial_date()
+
+        search_filter = {
+            "$or": [
+                {
+                    "last_created_date": {
+                        "$lt": today
+                    }
+                },
+                {
+                    "last_created_date": {
+                        "$exists": False
+                    }
+                }
+            ],
+            "is_active": True,
+            "is_enabled": True
+        }
+
+        routines = self._dao.get_by_filter(search_filter)
+
+        return routines
+
+    def get_by_id(self, id):
+        logger.info("Initializing get by id")
+        routine = self._dao.get({ "id": id })
+
+        return routine
+
+    def update_last_created_date(self, routine_id, date):
+        logger.info("Updating date in routine {0}".format(routine_id))
+        self._dao.update_last_created_date(routine_id, date)
